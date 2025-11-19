c81354e0cceae16b8038a3fb16455743e003ce86
Arceo <mail.arceo.me@gmail.com>
2025-11-20 05:30:47 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -6,9 +6,28 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/pxpic', async (req, res) => {
+    try {
+        const params = new URLSearchParams(req.body);
+        const response = await fetch("https://pxpic.com/callAiFunction", {
+            method: "POST",
+            headers: { 
+                'Content-Type': 'application/x-www-form-urlencoded' 
+            },
+            body: params
+        });
+
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'main.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
